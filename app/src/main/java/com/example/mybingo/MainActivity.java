e97c81b5287c3b4ebceb08b12f493fa56ecbaf27
t179a <azmtzk179@gmail.com>
2021-03-18 19:03:31 +0900

    create variable for　maxNumberEditText and registerMaxNumberButton.

--- app/src/main/java/com/example/mybingo/MainActivity.java
+++ app/src/main/java/com/example/mybingo/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     //最大値
     private int maxNumber = 75;
+
+    //最大の入力欄
+    private EditText maxNumberEditText;
+
+    //最大値の設定ボタン
+    private Button registerMaxNumberButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
